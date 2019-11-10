dffe35b6ae814441344887bcb17c8247c3136700
Umberto Palazzo <upalazzo93@gmail.com>
2019-11-10 22:05:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! Ya bishhh "
+    "Welcome to your app!!!! Ya bishhh \n corn "
   end
 
 end
